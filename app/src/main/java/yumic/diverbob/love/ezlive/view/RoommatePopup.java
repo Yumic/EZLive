613e310ab77d0f1dc7bff351d90ad5c0470eb248
Yumic <lightning201@qq.com>
2015-10-24 19:55:39 +0800

    1.修正MainActivity

--- app/src/main/java/yumic/diverbob/love/ezlive/view/RoommatePopup.java
+++ app/src/main/java/yumic/diverbob/love/ezlive/view/RoommatePopup.java
@@ -1,11 +1,61 @@
 package yumic.diverbob.love.ezlive.view;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AbsListView;
 import android.widget.PopupWindow;
+
+import yumic.diverbob.love.ezlive.R;
 
 /**
  * Created by Oathkeeper on 2015/10/24.
  */
 public class RoommatePopup extends PopupWindow{
+
+    private View contentView;
+
+    public RoommatePopup(Activity activity){
+        LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        contentView = inflater.inflate(R.layout.popupwindow_findroommate, null);
+        int h = activity.getWindowManager().getDefaultDisplay().getHeight();
+
+        int w = activity.getWindowManager().getDefaultDisplay().getWidth();
+        // 设置SelectPicPopupWindow的View
+        this.setContentView(contentView);
+        // 设置SelectPicPopupWindow弹出窗体的宽
+        this.setWidth(w - 4);
+        // 设置SelectPicPopupWindow弹出窗体的高
+        this.setHeight(AbsListView.LayoutParams.WRAP_CONTENT);
+        // 设置SelectPicPopupWindow弹出窗体可点击
+        this.setFocusable(true);
+        this.setOutsideTouchable(true);
+        // 刷新状态
+        this.update();
+        // 实例化一个ColorDrawable颜色为半透明
+        ColorDrawable dw = new ColorDrawable(0000000000);
+        // 点back键和其他地方使其消失,设置了这个才能触发OnDismisslistener ，设置其他控件变化等操作
+        this.setBackgroundDrawable(dw);
+        // mPopupWindow.setAnimationStyle(android.R.style.Animation_Dialog);
+        // 设置SelectPicPopupWindow弹出窗体动画效果
+        this.setAnimationStyle(R.style.AnimationPreview);
+
+    }
+
+    public void showPopupWindow(View parent) {
+        if (!this.isShowing()) {
+            this.showAsDropDown(parent, parent.getLayoutParams().width -10, 18);
+        } else {
+            this.dismiss();
+        }
+    }
+
+
+
+
+
 
 
 }
