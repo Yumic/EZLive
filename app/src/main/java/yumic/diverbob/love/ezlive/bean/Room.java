5aeea11043d7aef45c648864485634155591ddf3
draftbk <971143573@qq.com>
2015-10-24 19:36:10 +0800

    增加Room类

---
+++ app/src/main/java/yumic/diverbob/love/ezlive/bean/Room.java
@@ -0,0 +1,255 @@
+package yumic.diverbob.love.ezlive.bean;
+
+/**
+ * Created by apple on 15/10/24.
+ */
+public class Room {
+
+    /**
+     * 最小年龄
+     */
+    private String age_min;
+
+    /**
+     * 最大年龄
+     */
+    private String age_max;
+
+    /**
+     * 附近的东西
+     */
+    private String around;
+
+    /**
+     * 描述
+     */
+    private String describe;
+
+    /**
+     * 空格
+     */
+    private String flag;
+
+    /**
+     * 楼层
+     */
+    private String floor;
+
+    /**
+     * id
+     */
+    private String id;
+
+    /**
+     * 我的描述
+     */
+    private String owner_describe;
+
+    /**
+     * 我的标签
+     */
+    private String owner_tags;
+
+
+    /**
+     * 位置
+     */
+    private String position;
+
+    /**
+     * 与position一起拼成完整地
+     */
+    private String position_more;
+
+    /**
+     * 最低价格
+     */
+    private String price_min;
+
+    /**
+     * 最高价格
+     */
+    private String price_man;
+
+    /**
+     * 房间主人
+     */
+    private String room_owner;
+
+    /**
+     * 性别
+     */
+    private String sex;
+
+    /**
+     *平方
+     */
+    private String square;
+
+    /**
+     *标签
+     */
+    private String tags;
+
+    /**
+     *类型
+     */
+    private String type;
+
+    /**
+     *期望的描述
+     */
+    private String wish_describe;
+
+    public String getAge_min() {
+        return age_min;
+    }
+
+    public void setAge_min(String age_min) {
+        this.age_min = age_min;
+    }
+
+    public String getAge_max() {
+        return age_max;
+    }
+
+    public void setAge_max(String age_max) {
+        this.age_max = age_max;
+    }
+
+    public String getAround() {
+        return around;
+    }
+
+    public void setAround(String around) {
+        this.around = around;
+    }
+
+    public String getDescribe() {
+        return describe;
+    }
+
+    public void setDescribe(String describe) {
+        this.describe = describe;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+
+    public String getFloor() {
+        return floor;
+    }
+
+    public void setFloor(String floor) {
+        this.floor = floor;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOwner_describe() {
+        return owner_describe;
+    }
+
+    public void setOwner_describe(String owner_describe) {
+        this.owner_describe = owner_describe;
+    }
+
+    public String getOwner_tags() {
+        return owner_tags;
+    }
+
+    public void setOwner_tags(String owner_tags) {
+        this.owner_tags = owner_tags;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getPosition_more() {
+        return position_more;
+    }
+
+    public void setPosition_more(String position_more) {
+        this.position_more = position_more;
+    }
+
+    public String getPrice_min() {
+        return price_min;
+    }
+
+    public void setPrice_min(String price_min) {
+        this.price_min = price_min;
+    }
+
+    public String getPrice_man() {
+        return price_man;
+    }
+
+    public void setPrice_man(String price_man) {
+        this.price_man = price_man;
+    }
+
+    public String getRoom_owner() {
+        return room_owner;
+    }
+
+    public void setRoom_owner(String room_owner) {
+        this.room_owner = room_owner;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getSquare() {
+        return square;
+    }
+
+    public void setSquare(String square) {
+        this.square = square;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getWish_describe() {
+        return wish_describe;
+    }
+
+    public void setWish_describe(String wish_describe) {
+        this.wish_describe = wish_describe;
+    }
+}
